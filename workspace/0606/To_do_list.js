2ed55344c2e164fc84608a0b2d0c8e659ecf85bb
jhoon12 <yukihoon12@naver.com>
2020-06-06 23:04:12 +0900

    0604(To_DO_List)
    블로그를 보고 공부해 정확히 이해하지는 못했습니다. 내일 코드 수정해서 다시 올릴 예정입니다.

---
+++ workspace/0606/To_do_list.js
@@ -0,0 +1,55 @@
+const toDoForm = document.querySelector('.js-toDoForm'),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector('.js-toDoList');
+
+const TODOS_LS = 'toDos';
+
+let toDos = [];  
+function saveToDos () {  
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+
+function paintToDo(text) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.innerHTML = text;
+    li.appendChild(span);
+    toDoList.appendChild(li);
+    
+    const toDoObj = {
+        text : text,
+        id : toDos.length+1
+    }
+
+    toDos.push(toDoObj);
+    saveToDos(toDos);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const toDoText = toDoInput.value;
+    paintToDo(toDoText);
+    toDoInput.value="";
+    
+    
+}
+
+function loadToDolist() {
+    const getList = JSON.parse(localStorage.getItem(TODOS_LS));
+    if(getList === null){
+
+    }else{
+        getList.forEach(function(todo){
+            paintToDo(todo.text);
+        })
+    }
+}
+
+function init() {
+    loadToDolist();
+    toDoForm.addEventListener('submit',handleSubmit);
+
+}
+
+init();
