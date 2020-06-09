400f4014a9067ae4154a1284230882b825bd8318
jhoon12 <yukihoon12@naver.com>
2020-06-09 23:16:27 +0900

    0609
    강의를 들으며 공부한 기록입니다.

---
+++ workspace/0610/clock.js
@@ -0,0 +1,13 @@
+const clockContainer = document.querySelector("".js-clock),
+      clockTitle = clockContainer.querySelector("h1");
+
+function getTime(){
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    clock 
+}
+
+function init(){
+}
+init();
