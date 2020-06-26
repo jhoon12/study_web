f0e070678deca8dbf33f33065d7bae3c3a319079
jhoon12 <yukihoon12@naver.com>
2020-06-26 20:18:17 +0900

    0626
    회원가입시 나오는 질문에 대한 코드와 서버통신입니다

--- small_pro/Stones/Goal.js
+++ small_pro/Stones/Goal.js
@@ -1,4 +1,44 @@
+// const { default: Axios } = require("axios");
 const Goal = document.getElementById("Goal");
 const term = document.getElementById("term");
-const SetTime = document.getElementById("SetTime");
+const setTime = document.getElementById("SetTime");
+const btn = document.getElementById("btn");
 
+
+// let arr = new Array();
+// const date = term.value;
+// arr = date.split("-");
+btn.onclick = async function(){
+    const date = term.value;
+    const setTime = SetTime.value;
+    
+    const goal = Goal.value;
+
+    let dateArr = new Array();
+    dateArr = date.split("-");
+        const year = dateArr[0];
+        const month = dateArr[1];
+        const day = dateArr[2];
+
+    let timeArr = new Array();
+        timeArr = setTime.split(":");
+        const time = timeArr[0];
+        const minute = timeArr[1]; 
+   
+    try{
+        const res = await axios({
+        method:'post',
+        url: 'http://13.209.77.9:3000/api/auth/register',
+        data : {
+            year : year,
+            month : month,
+            day : day,
+            do : goal,
+        }
+        })
+        console.log('전송완료');
+    }catch(err) {
+        console.log(err);
+    }
+ 
+};
