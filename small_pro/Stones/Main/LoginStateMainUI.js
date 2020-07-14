3a13f441255f97de13ebb5feef9c0da006fe807e
jhoon12 <yukihoon12@naver.com>
2020-07-15 08:23:23 +0900

    0715
    완료하기 버튼 기능 구현(50%)

--- small_pro/Stones/Main/LoginStateMainUI.js
+++ small_pro/Stones/Main/LoginStateMainUI.js
@@ -1,4 +1,3 @@
-
 const NameText = document.getElementById("NameText");
 const TimeText = document.getElementById("TimeText");
 const stonesExpression = document.getElementById("stonesExpression");
@@ -7,17 +6,47 @@ const todo = document.getElementById("todo");
 document.querySelectorAll(".toDoCheck").forEach(element => element.onclick = changeStyle);
 document.getElementById("success").onclick = submitPlan;
 document.getElementById("add").onclick = addPlan;
-let arr = [8];
+let arr = new Array(8);
 let TextArr = [];
+const clickedArray = [];
 
-async function submitPlan(){
-  const res = await axios({
-    method : 'get',
-    url : `${CONSTANT.SERVER_ADRESS}/main`,
-    body:{
-    }
-  });
-} 
+function submitPlan(){
+  // if(querySelectorAll(TextArr).forEach((TexArr, index)=> {if(TextArr[index] !== null){
+  //     document.querySelectorAll(".toDoCheck").forEach(async element =>{
+  //       if(element.classList.toggle("active")){
+  //         const time = new Date();
+  //         const year = time.getFullYear();
+  //         const month = time.getMonth();
+  //         const day = time.getDate();
+  //         try{const res = await axios({
+  //           method : 'get',
+  //           url : `${CONSTANT.SERVER_ADRESS}/main`,
+  //           headers:{
+  //             access_token : localStorage.access_token
+  //           },
+  //           body:{
+  //             year,
+  //             month,
+  //             day
+  //           }
+  //         });
+  //         if(res.status === 200)
+  //           console.log('성공');
+  //         }catch(err){
+  //             console.log(err);
+  //             console.log(err.response.status);
+  //         }
+  //       }
+  //     })
+  //   }
+  // })
+  // );
+    const activedInputLength = Array.from(document.querySelectorAll("input.input")).filter(ele => ele.value !== "").length;
+    const activeButtonLength = document.querySelectorAll(".toDoCheck.active").length;
+    console.log(activedInputLength,activeButtonLength);
+
+}
+ 
 async function addPlan(){
   document.querySelectorAll(".input").forEach(text => {
     TextArr.push(text.value);
@@ -26,12 +55,12 @@ async function addPlan(){
     const res = await axios({
       method : 'post',
       url : `${CONSTANT.SERVER_ADRESS}/todo`,
-      data :{
+      data :{ 
         what : TextArr,
       },
       headers:{
         access_token : localStorage.access_token
-      } 
+      }   
     });
     window.location.reload();
   } catch(err) {
@@ -40,6 +69,7 @@ async function addPlan(){
 }
 
 function changeStyle() {
+  if(this.parentElement.querySelector("input").value === "") return;
   this.classList.toggle("active");//클래스가 있으면 추가, 없으면 추가 X
 }
 
@@ -56,15 +86,12 @@ window.onload = async ()=>{
         const replaceStr = res.data.phrase_word.replace(/\./g,".\n");//정규표현식
         stonesExpression.innerText = `"${replaceStr}"\n -${res.data.phrase_name}-`;
         NameText.innerText = `${res.data.name}님`;
-        
+
         const todoArr = document.querySelectorAll('.todo');
-        if(res.data.todo_what) {
-          res.data.todo_what.forEach((arrElements,index) =>{
-            todoArr[index].innerHTML = `<input class="input" value=${arrElements} >`;
-            console.log(todoArr[index]);
-          });
-        }
-        
+        res.data.todo_what.forEach((arrElements,index) =>{
+        todoArr[index].innerHTML = `<input class="input" value=${arrElements} >`;
+        console.log(todoArr[index]);
+      });
         if(res.data.hour < 12){
           TimeText.innerText = `오전 ${res.data.hour}:${res.data.minute}까지 계획을 완료해야 합니다!`;
         }
@@ -78,13 +105,9 @@ window.onload = async ()=>{
       }
   catch(e){ 
     console.log(e);
-    if(e.response.status === 500){
-      console.log("실패");
-    }
+    if(e.response.status === 500)
+      console.log("실패");  
   }
-
-  
-
 }
 
 logout.onclick =()=>{
@@ -92,4 +115,3 @@ logout.onclick =()=>{
     localStorage.removeItem('refresh_token');
     window.location.href = "../Login/Login.html";
 }
-
